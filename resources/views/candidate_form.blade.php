20091440dafc91fe97d345c30d982b49f66abbea
Viktor <wbdev.vox@gmail.com>
2025-12-18 12:50:41 +0400

    add README

--- resources/views/candidate_form.blade.php
+++ resources/views/candidate_form.blade.php
@@ -2,12 +2,12 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Работа мечты (Laravel)</title>
+    <title>Отклик на вакансию</title>
     <script src="https://cdn.tailwindcss.com"></script> </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
 
     <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">Откликнуться (Laravel)</h1>
+        <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">Откликнуться</h1>
 
         @if(session('success'))
             <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">
